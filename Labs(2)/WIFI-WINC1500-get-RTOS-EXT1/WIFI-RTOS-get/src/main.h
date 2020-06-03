cca0d523b45308b5cf46c64b57ba47fa28416d8f
mikomoares <mihanoch@gmail.com>
2020-06-03 16:06:04 -0300

    sim

--- Labs(2)/WIFI-WINC1500-get-RTOS-EXT1/WIFI-RTOS-get/src/main.h
+++ Labs(2)/WIFI-WINC1500-get-RTOS-EXT1/WIFI-RTOS-get/src/main.h
@@ -48,9 +48,9 @@ extern "C" {
 #include "driver/include/m2m_wifi.h"
 
 /** Wi-Fi Settings */
-#define MAIN_WLAN_SSID                    "Peridot" /**< Destination SSID */
+#define MAIN_WLAN_SSID                    "Sniffer" /**< Destination SSID */
 #define MAIN_WLAN_AUTH                    M2M_WIFI_SEC_WPA_PSK /**< Security manner */
-#define MAIN_WLAN_PSK                     "moraes842" /**< Password for Destination SSID */
+#define MAIN_WLAN_PSK                     "sim" /**< Password for Destination SSID */
 
 /** Using broadcast address for simplicity. */
 #define MAIN_SERVER_PORT                    (5000)
